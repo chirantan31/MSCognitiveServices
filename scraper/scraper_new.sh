121a2637649653a63cb38e02e4d1831ff0bde7e1
Hongyu Wang <hongyuw2@illinois.edu>
2018-09-30 16:03:47 -0500

    audio scraper with correct version

--- scraper/scraper_new.sh
+++ scraper/scraper_new.sh
@@ -1,13 +1,13 @@
 IFS=$'\n' read -d '' -r -a args < args.txt
 echo Start getting audio urls
 $play_session=""
-curl 'https://echo360.org/section/0f681b91-e357-4c4b-9607-4e2e302d2245/syllabus' -H "Cookie: PLAY_SESSION=8961c8563522d5688b547963849b2d999bdc0fc5-role=Instructor&institution=d422df75-c848-4e5e-b375-e46893d4de8a&classesGraphId=&questionQuery=%7B%22sectionId%22%3A%220f681b91-e357-4c4b-9607-4e2e302d2245%22%2C%22lessonId%22%3A%22G_fc164d0a-fc39-46f6-a95f-7b6755bb995e_0f681b91-e357-4c4b-9607-4e2e302d2245_2018-09-14T09%3A00%3A00.000_2018-09-14T09%3A53%3A00.000%22%2C%22sortDirection%22%3A%22desc%22%2C%22isClassroom%22%3Atrue%2C%22pageNumber%22%3A0%7D&roles=Instructor%2CStudent&csrfToken=43afcf0ea29e1778df19a16b5ce98129edab2edc-1538025010593-848003bf49dcf75333695789&user=2143bda1-7bed-4aac-ac6b-bbb15c2e86f2;"  --compressed | python -m json.tool | grep "s3Url" | grep "audio" | awk  -F": |\"" '{print $5}' > audio_url_temp.txt
+curl "https://echo360.org/section/${args[0]}/syllabus" -H "Cookie: PLAY_SESSION=8961c8563522d5688b547963849b2d999bdc0fc5-role=Instructor&institution=d422df75-c848-4e5e-b375-e46893d4de8a&classesGraphId=&questionQuery=%7B%22sectionId%22%3A%220f681b91-e357-4c4b-9607-4e2e302d2245%22%2C%22lessonId%22%3A%22G_fc164d0a-fc39-46f6-a95f-7b6755bb995e_0f681b91-e357-4c4b-9607-4e2e302d2245_2018-09-14T09%3A00%3A00.000_2018-09-14T09%3A53%3A00.000%22%2C%22sortDirection%22%3A%22desc%22%2C%22isClassroom%22%3Atrue%2C%22pageNumber%22%3A0%7D&roles=Instructor%2CStudent&csrfToken=43afcf0ea29e1778df19a16b5ce98129edab2edc-1538025010593-848003bf49dcf75333695789&user=2143bda1-7bed-4aac-ac6b-bbb15c2e86f2;"  --compressed | python -m json.tool | grep "s3Url" | grep "audio" | awk  -F": |\"" '{print $5}' > audio_url_temp.txt
 echo $play_session
 IFS=$'\n' read -d '' -r -a urls < audio_url_temp.txt
 echo Start downloading audio
 n=1
 for url in ${urls[@]}
 do
-    wget --header="Cookie: CloudFront-Key-Pair-Id=${args[0]}; CloudFront-Policy=${args[1]}; CloudFront-Signature=${args[2]}" --header="Connection: keep-alive" "${url}" -O "audio${n}.mp3" -c
+    wget --header="Cookie: CloudFront-Key-Pair-Id=${args[1]}; CloudFront-Policy=${args[2]}; CloudFront-Signature=${args[3]}" --header="Connection: keep-alive" "${url}" -O "audio${n}.mp3" -c
     ((n+=1))
 done
